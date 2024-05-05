90fe92234a02abc7f938b93d3662c1beef0548d3
José Lúcio <joselucioosjunior@gmail.com>
2024-05-05 20:49:17 -0300

    Tipos Primitivos e tipamento da linguagem

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // byte; --> [-128-127] (parte inteira)
+        //short; --> [-32768-32767] (parte inteira)
+        //int; --> [-2147483648-2147483647] (parte inteira)
+        //long; --> [900000000000000000] (parte inteira)
+        //boolean; --> true or false 
+        //char; --> 'M' (um caracter)
+        //float; --> [muitos numeros] (parte fracionária)
+        //double. [abriga mais números que o float] (parte fracionária)
+        // variável 'final' = constante; recomendado usar letras maiúsculas para representar final variable; 
+        // para declaração de variável 'long' precisa adotar a terminação 'L/l', o mesmo para variável 'float', necessário adotar terminação 'F/f'
+        // java é fortemente tipado, isso é : requer declarar as variáveis com seu tipo de dados. Ex: String, int, long, float, etc...;
+        
     }
 }
