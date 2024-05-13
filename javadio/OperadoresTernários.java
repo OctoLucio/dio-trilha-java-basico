4b8ca075e0929f4367658460659e2dd0bb38e43b
José Lúcio <joselucioosjunior@gmail.com>
2024-05-13 16:32:26 -0300

    .

--- javadio/OperadoresTernários.java
+++ javadio/OperadoresTernários.java
@@ -1,7 +1,7 @@
 public class OperadoresTernários {
     public static void main(String[] args) {
         int nota = 8;
-        String sit = (nota>=7)?"aprovado":"recuperação";
+        String sit = (nota>=7)?"Aprovado": (nota>=5 && nota<7)?"Recuperação":"Reprovado";
         System.out.println(sit);
     }
 }
