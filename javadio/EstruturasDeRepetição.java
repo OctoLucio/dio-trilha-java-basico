70ff74c260375b50c631b5ed30baca1bedaa1e95
José Lúcio <joselucioosjunior@gmail.com>
2024-05-14 14:30:01 -0300

    .

--- javadio/EstruturasDeRepetição.java
+++ javadio/EstruturasDeRepetição.java
@@ -2,7 +2,7 @@ public class EstruturasDeRepetição{
 
     public static void main(String[] args) {
         String alunos[] = {"Felipe", "José", "Ana", "Cláudia", "Gabriel"};
-        for(int x=1; x < alunos.length; x++) {
+        for(int x=0; x < alunos.length; x++) {
             System.out.println("O aluno " + x + " é " + alunos[x]);
     }   
     }
